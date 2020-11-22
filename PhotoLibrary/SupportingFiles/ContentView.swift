3b2179e5789679c7ee4f38c6a89f75048bc3a790
henadark <henadark@mail.ru>
2020-11-22 22:49:05 +0200

    first commit

---
+++ PhotoLibrary/SupportingFiles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoLibrary
+//
+//  Created by Hena Knyzhnyk on 22.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoLibrary/SupportingFiles/PhotoLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLibraryApp.swift
+//  PhotoLibrary
+//
+//  Created by Hena Knyzhnyk on 22.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
